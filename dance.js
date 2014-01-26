b4a36cc213a37f658b30e04a1d4865305857ca53
cl <cl@startup.org.sg>
2014-01-26 10:42:31 +0800

    initial - outline

---
+++ dance.js
@@ -0,0 +1,53 @@
+initDanceWorld();
+setDanceRoutine();
+
+//==================================================
+function initDanceWorld(){
+  //Create and Set All Canvases
+}//endof initLockscreen()
+
+
+
+//==================================================
+function setDanceRoutine(){
+  var dance = {
+    arrows: [],
+    speeds: [],
+  };
+
+  var iter = 0;
+  //When start game
+  //Set Interval...
+    if (iter<dance.arrows.length){
+      iter += 1;
+      createMovingArrow(dance.arrows[iter], dance.speeds[iter]);
+    }
+
+}
+
+
+
+//==================================================
+function createMovingArrow(directionString,speed){
+
+}
+
+//==================================================
+function drawArrow(directionString){}
+function drawArrowBay(directionString, coord){ //the Bay that Moving Arrows will go into
+
+}
+
+//==================================================
+function addHandTracker(directionString){
+  //directionString can be either the regular direction string, or an object with coordinates
+}
+function delHandTracker(directionString){}
+
+function createCoord(xx,yy){
+  return {x:xx, y:yy};
+}
+
+
+//==================================================
+//Utilities
