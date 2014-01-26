6ec3913d8bf86f4d26460f96cd305218f0907483
cl <cl@startup.org.sg>
2014-01-26 10:50:01 +0800

    added canvases

--- dance.js
+++ dance.js
@@ -4,8 +4,43 @@ setDanceRoutine();
 //==================================================
 function initDanceWorld(){
   //Create and Set All Canvases
+  var worldBkgdCanvas = document.getElementById("worldBkgdCanvas");
+  var whiteMaskCanvas = document.getElementById("whiteMaskCanvas");
+
+  var arrowBayCanvas = document.getElementById("arrowBayCanvas");
+  var tempArrowBayCanvas = document.getElementById("tempArrowBayCanvas");
+  
+  var movingArrowCanvas = document.getElementById("movingArrowCanvas");
+
+  setSizeDanceWorld();
+
+  // clickCanvas.width = 100; clickCanvas.height = 100;
+  // glowingCanvas.width = 100; glowingCanvas.height = 100;
+  // scoreCanvas.width = 100; scoreCanvas.height = 100;
+
+  $(window).resize(function() {
+    setSizeDanceWorld(); ///worldBkgdCanvas.width = window.innerWidth; worldBkgdCanvas.height = window.innerHeight;
+    if(window.showResults_on){
+      // worldBkgdCanvas.getContext("2d").fillStyle = "rgba(0,0,0,0.7)";
+      // worldBkgdCanvas.getContext("2d").fillRect(0,0,worldBkgdCanvas.width,worldBkgdCanvas.height);
+    }
+  });
+
+  //Now create the permanent circles for clickCanvas and glowingCanvas.
+  //For scoreCanvas, it is not permanent as it will be overwritten.
+  //Hence create new empty copy of 3 hollow circles only when you start the screen saver.
+  var canvasCenter_coord = calcCanvasCenter(clickCanvas);
+  createCircle( clickCanvas, canvasCenter_coord, {r:0,g:0,b:0,a:-0.2},{a:-0.5} );
+  createCircle( glowingCanvas, canvasCenter_coord, {r:0,g:0,b:0,a:-0.2},{a:-0.7} );
 }//endof initLockscreen()
 
+function setSizeDanceWorld(){
+  worldBkgdCanvas.width = window.innerWidth; worldBkgdCanvas.height = window.innerHeight;
+  whiteMaskCanvas.width = window.innerWidth; whiteMaskCanvas.height = window.innerHeight;
+  arrowBayCanvas.width = window.innerWidth; arrowBayCanvas.height = window.innerHeight;
+  tempArrowBayCanvas.width = window.innerWidth; tempArrowBayCanvas.height = window.innerHeight;
+  movingArrowCanvas.width = window.innerWidth; movingArrowCanvas.height = window.innerHeight;
+}
 
 
 //==================================================
